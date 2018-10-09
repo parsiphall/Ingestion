cb0f0dc1f055314c0d831b740a63c27d00742b75
parsiphall <parsiphall@gmail.com>
2018-10-09 23:35:27 +0300

    base arch

--- app/src/main/java/com/example/parsiphal/ingestion/model/StandartModel.kt
+++ app/src/main/java/com/example/parsiphal/ingestion/model/StandartModel.kt
@@ -1,6 +1,7 @@
 package com.example.parsiphal.ingestion.model
 
-data class StandartModel(var date: String,
+data class StandartModel(var type: Int,
+                         var date: String,
                          var weight: String,
                          var breakfast: String,
                          var snack1: String,
